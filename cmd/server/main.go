94955ad2770458442e924dc0875d5faf1aaa14de
Dione Vianna <dioneg12@hotmail.com>
2025-03-05 00:07:18 -0300

    :recycle: validation adjustment

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -43,7 +43,7 @@ func startServer(provider queue.QueueProvider, config map[string]string) error {
 }
 
 func main() {
-	err := godotenv.Load("./")
+	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
 		panic("Error loadin .env file")
--- internal/order/validation/order_validation.go
+++ internal/order/validation/order_validation.go
@@ -1,28 +1,29 @@
 package validation
 
 import (
+	"fmt"
 	proto "order_open_delivery_go_service/api/order-proto"
 
 	"github.com/go-playground/validator"
 )
 
 type ItemValidation struct {
-	Id                string  `validate:"required"`
-	Index             int32   `validate:"required,gt=0"`
-	Name              string  `validate:"required"`
-	ExternalCode      string  `validate:"required"`
-	Unit              string  `validate:"required"`
-	Ean               string  `validate:"required"`
-	Quantity          int32   `validate:"required,gt=0"`
-	SpecialInstructions string `validate:"omitempty"`
-	UnitPriceValue    float32 `validate:"required,gt=0"`
-	OptionsPriceValue float32 `validate:"required,gt=0"`
-	TotalPriceValue   float32 `validate:"required,gt=0"`
+	Id                  string  `validate:"required"`
+	Index               int32   `validate:"required,gt=0"`
+	Name                string  `validate:"required"`
+	ExternalCode        string  `validate:"required"`
+	Unit                string  `validate:"required"`
+	Ean                 string  `validate:"required"`
+	Quantity            int32   `validate:"required,gt=0"`
+	SpecialInstructions string  `validate:"omitempty"`
+	UnitPriceValue      float32 `validate:"required,gt=0"`
+	OptionsPriceValue   float32 `validate:"required,gt=0"`
+	TotalPriceValue     float32 `validate:"required,gt=0"`
 }
 
 type OrderRequestValidation struct {
 	Id    string        `validate:"required"`
-	Items []*proto.Item `validate:"required,dive"` 
+	Items []*proto.Item `validate:"required,dive"`
 }
 
 func NewOrderRequestValidation() *OrderRequestValidation {
@@ -36,31 +37,60 @@ func NewItemValidation() *ItemValidation {
 func ValidateOrderRequest(orderRequest *proto.OrderRequest) error {
 	validate := validator.New()
 
-	// OrderRequest Validation
+	if orderRequest == nil {
+		return fmt.Errorf("orderRequest is nil")
+	}
+
+	if orderRequest.Items == nil {
+		return fmt.Errorf("orderRequest.Items is nil")
+	}
+
 	orderValidation := &OrderRequestValidation{
 		Id:    orderRequest.Id,
 		Items: orderRequest.Items,
 	}
 
-	// Validates the OrderRequest itself
 	if err := validate.Struct(orderValidation); err != nil {
 		return err
 	}
 
-	// Validation of items (each item in the list)
 	for _, item := range orderRequest.Items {
+		if item == nil {
+			return fmt.Errorf("found nil item in orderRequest.Items")
+		}
+
+		var unitPriceValue, optionsPriceValue, totalPriceValue float32
+
+		if item.UnitPrice != nil {
+			unitPriceValue = item.UnitPrice.Value
+		} else {
+			return fmt.Errorf("item.UnitPrice is nil")
+		}
+
+		if item.OptionsPrice != nil {
+			optionsPriceValue = item.OptionsPrice.Value
+		} else {
+			return fmt.Errorf("item.OptionsPrice is nil")
+		}
+
+		if item.TotalPrice != nil {
+			totalPriceValue = item.TotalPrice.Value
+		} else {
+			return fmt.Errorf("item.TotalPrice is nil")
+		}
+
 		itemValidation := &ItemValidation{
-			Id:                item.Id,
-			Index:             item.Index,
-			Name:              item.Name,
-			ExternalCode:      item.ExternalCode,
-			Unit:              item.Unit,
-			Ean:               item.Ean,
-			Quantity:          item.Quantity,
+			Id:                  item.Id,
+			Index:               item.Index,
+			Name:                item.Name,
+			ExternalCode:        item.ExternalCode,
+			Unit:                item.Unit,
+			Ean:                 item.Ean,
+			Quantity:            item.Quantity,
 			SpecialInstructions: item.SpecialInstructions,
-			UnitPriceValue:    item.UnitPrice.Value,
-			OptionsPriceValue: item.OptionsPrice.Value,
-			TotalPriceValue:   item.TotalPrice.Value,
+			UnitPriceValue:      unitPriceValue,
+			OptionsPriceValue:   optionsPriceValue,
+			TotalPriceValue:     totalPriceValue,
 		}
 
 		if err := validate.Struct(itemValidation); err != nil {
